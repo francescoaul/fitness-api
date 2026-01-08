e0620eed408aa2a60d95a3cd23b82031a0c425b8
Francesco Aulino <francesco.aul@outlook.com>
2026-01-08 11:15:31 -0500

    Fix auth flow and workouts API

--- app.js
+++ app.js
@@ -15,7 +15,18 @@ const EXERCISE_TYPES = ['cardio', 'strength', 'flexibility', 'balance'];
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors( { origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://localhost:5175",
+    "http://127.0.0.1:5173",
+    "http://127.0.0.1:5175",
+  ],
+  credentials: true
+}));
+
+
 app.use(cookieParser());
 
 app.get('/health', (request, response) => {
