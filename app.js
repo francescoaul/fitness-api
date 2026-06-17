5f548e545ac5544293ee5d1f18ae91aacb2dc6b1
Francesco <75341270+francescoaul@users.noreply.github.com>
2026-06-16 21:24:54 -0400

    Update app.js

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(cors({
     "http://localhost:5175",
     "http://127.0.0.1:5173",
     "http://127.0.0.1:5175",
+    "https://quick-flex-workout-app.vercel.app"
   ],
   credentials: true
 }));
